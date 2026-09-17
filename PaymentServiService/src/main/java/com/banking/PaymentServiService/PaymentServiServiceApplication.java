d7829236955023125f7dfe2a0bcdc1c5fc746b11
ghada <ghadamhenni123@gmail.com>
2026-09-17 13:20:04 +0200

    maven

---
+++ PaymentServiService/src/main/java/com/banking/PaymentServiService/PaymentServiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.PaymentServiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiServiceApplication.class, args);
+	}
+
+}
---
+++ PaymentServiService/src/test/java/com/banking/PaymentServiService/PaymentServiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.PaymentServiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
